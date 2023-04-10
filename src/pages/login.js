3465d8c1a8765945189597979e447d2336398cbf
nanak <sidhu.nds22@gmail.com>
2023-04-10 12:54:12 +0530

    added login with OAuth(Google)

---
+++ src/pages/login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Login_func from '../../components/Login_func'
+
+export default function login() {
+  return (
+    <div>
+      <Login_func></Login_func>
+    </div>
+  )
+}
