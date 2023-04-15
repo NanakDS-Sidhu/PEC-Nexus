40cbeeb8be7a0b3a6042ff225a1a38013fcb362b
nanak <sidhu.nds22@gmail.com>
2023-04-15 15:11:21 +0530

    Posts with images

---
+++ src/components/devblog/Card.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import supabase from '@/lib/SupabaseConfig';
+
+export default function Card(props) {
+    const [loading,setLoading]=useState(true);
+    const [img,setImage]=useState("")
+    async function getPostImage(){
+        let { data: Post_to_images, error } = await supabase
+            .from('Post_to_images')
+            .select('Image_url')
+            .eq('Post_id', props.post.id)
+            if(error){
+                console.log(error)
+            }else{
+                // console.log(Post_to_images)
+                if(Post_to_images.length !== 0){
+                    setImage(Post_to_images[0].Image_url)
+                }
+                setLoading(false)
+            }
+        }
+        useEffect(()=>{
+            getPostImage();
+        },[])
+    
+
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl m-4">
+    {loading?<h1>Loading</h1>:<figure><img src={img} alt="Shoes" /></figure>}
+    <div className="card-body">
+        <h2 className="card-title">{props.post.title}</h2>
+        <p>{props.post.description}</p>
+        <div className="card-actions justify-end">
+        <button className="btn btn-primary">Read More</button>
+        </div>
+    </div>
+    </div>
+  )
+}
--- src/pages/devblog.js
+++ src/pages/devblog.js
@@ -5,19 +5,40 @@ import BlogList from "@/components/devblog/BlogList";
 
 export default function devblogs(){
     const { register, handleSubmit} = useForm();
+
+    async function uploadFile(file) {
+        console.log(file[0])
+        const { data, error } = await supabase.storage
+        .from('Post_Images/JK')
+        .upload(file[0].name.toString(),file[0],{
+            upsert: false
+          })
+        if (error) {
+          // Handle error
+          console.log(error)
+        } else {
+          // Handle success
+          console.log("success")
+        }
+      }
+
+
     async function createPost(d){
-        try{
-            
         const { data: { user } } = await supabase.auth.getUser()
         const { data, error } = await supabase
         .from('Posts')
         .insert([
         { title: d.title , description: d.description,author_id:user.id},
-            ])}
-        catch(e){
-            console.log(e)
-        }
-        
+            ]).select()
+        uploadFile(d.image)
+
+        // console.log(data)
+        const { data2, error2 } = await supabase
+        .from('Post_to_images')
+        .insert([
+            { Post_id:data[0].id, Image_url:"https://djwsvisvriprqmnebcmz.supabase.co/storage/v1/object/public/Post_Images/JK/"+d.image[0].name.toString()},
+        ])
+
     }
 
     return (
@@ -26,6 +47,7 @@ export default function devblogs(){
             <form onSubmit={handleSubmit(createPost)}>
                 <input type="text" placeholder="title"  {...register("title")}></input>
                 <input type="text" placeholder="description" {...register("description")}></input>
+                <input type="file" {...register("image")}></input>
                 <button type="submit">Submit</button>
             </form>
             <BlogList></BlogList>
