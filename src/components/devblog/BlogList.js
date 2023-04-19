69352615fdabc2a1fdc3647b4553e2b41d0876b1
nanak <sidhu.nds22@gmail.com>
2023-04-20 03:16:12 +0530

    Minor changes to the posts

--- src/components/devblog/BlogList.js
+++ src/components/devblog/BlogList.js
@@ -4,13 +4,7 @@ import Card from "./Card";
 
 export default function BlogList() {
     const [d,setd]=useState([])
-    async function get_images(post){
-        let { data: Post_to_images, error } = await supabase
-        .from('Post_to_images') 
-        .select("Image_url")
-        .eq('Post_id',post.id)
-    }
-
+    
     async function getposts(){
             let { data: Posts, error } = await supabase
             .from('Posts')
