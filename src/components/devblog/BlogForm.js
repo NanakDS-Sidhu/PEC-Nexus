a281818fd859d889181cedfd1b5b593a109dfacc
nanak <sidhu.nds22@gmail.com>
2023-05-07 22:03:59 +0530

    changes to blog form

--- src/components/devblog/BlogForm.js
+++ src/components/devblog/BlogForm.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import supabase from "@/lib/SupabaseConfig";
 
 export default function BlogForm() {
   const { register, handleSubmit} = useForm();
-
+    const [loading,setLoading]=useState(false)
     const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
       function generateString(length) {
@@ -35,6 +35,11 @@ export default function BlogForm() {
 
 
     async function createPost(d){
+      setLoading(true)
+      if(d.title===""){
+        alert("Title can not be empty")
+      }
+      else{
         const random_string=generateString(6)
         const { data: { user } } = await supabase.auth.getUser()
         const { data, error } = await supabase
@@ -51,70 +56,34 @@ export default function BlogForm() {
         .insert([
             { Post_id:data[0].id, Image_url:"https://djwsvisvriprqmnebcmz.supabase.co/storage/v1/object/public/Post_Images/JK/"+random_string+d.image[0].name.toString()},
         ])}
+      }
+        setLoading(false)
         }
 
   return (<>
-<div className="flex items-center justify-center p-12">
-
-  <div className="mx-auto w-full max-w-[550px] shadow-2xl">
-    <form
-      className="py-6 px-9"
-      onSubmit={handleSubmit(createPost)}
-    >
-      <div className="mb-5">
-        <label
-          htmlFor="text"
-          className="mb-3 block text-xl font-semibold text-[#07074D] text-left "
-        >
-          Title
-        </label>
-        <input type="text" placeholder="title" className="input input-bordered input-primary w-full max-w-xs" {...register("title")}/>
-      </div>
-      <div className="mb-5">
-        <label
-          htmlFor="text"
-          className="mb-3 block text-xl font-semibold text-[#07074D] text-left "
-        >
-          Description
-        </label>
-        <input type="text" placeholder="title" className="input input-bordered input-primary w-full max-w-xs" {...register("title")}/>
-      </div>
-
-      <div className="mb-6 pt-4">
-        <label className="mb-5 block text-xl font-semibold text-[#07074D]">
-          Upload File
-        </label>
-
-        <div className="mb-8">
-          <input type="file" name="file" id="file" {...register("image")} className="sr-only"/>
-          <label
-            htmlFor="file"
-            className="relative flex min-h-[200px] items-center justify-center rounded-md border border-dashed border-[#e0e0e0] p-12 text-center"
-          >
-            <div>
-              <span className="mb-2 block text-xl font-semibold text-[#07074D]">
-                Drop files here
-              </span>
-              <span className="mb-2 block text-base font-medium text-[#6B7280]">
-                Or
-              </span>
-              <span
-                className="inline-flex rounded border border-[#e0e0e0] py-2 px-7 text-base font-medium text-[#07074D]"
-              >
-                Browse
-              </span>
-            </div>
-          </label>
-        </div>
-
-
-
-      </div>
-
-      <button className="text-center text-base font-semibold btn btn-primary px-32" type="submit">Submit</button>
-    </form>
+<div className="flex items-center justify-center p-12 my-14">
+  <form className="card w-96 bg-base-300 text-primary-content" onSubmit={handleSubmit(createPost)}>
+    <h1 className="text-base-content text-center mt-4 text-xl text"> CREATE POST! </h1>
+  <div className="card-body items-center text-center">
+    <div className="form-control w-full max-w-xs">
+    <label className="label">
+      <span className="label-text">Enter Title</span>
+    </label>
+    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" {...register("title")}/>
   </div>
+  <div className="form-control w-full max-w-xs">
+    <label className="label">
+      <span className="label-text">Enter Description</span>
+    </label>
+    <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" {...register("description")}/>
+  </div>
+  <input type="file" className="file-input file-input-bordered file-input-primary w-full max-w-xs my-3" {...register("image")}/>
+  <button className="btn btn-primary w-3/4" disabled={loading} type="submit">{loading?"Loading":"Submit"}</button>
+  </div>
+</form>
 </div>
+
+
   </>
 
     
