f91a2309f7a79329d5a64a1e4cf87fdb7e08ae47
nanak <sidhu.nds22@gmail.com>
2023-05-07 20:31:19 +0530

    mad navbar glassy

--- src/pages/devblog/index.js
+++ src/pages/devblog/index.js
@@ -1,8 +1,5 @@
-import React, { useEffect } from "react";
-import { useForm } from "react-hook-form";
-import supabase from "@/lib/SupabaseConfig";
 import BlogList from "@/components/devblog/BlogList";
-import BlogForm from "@/components/devblog/BlogForm";
+
 
 
 export default function devblogs(){
