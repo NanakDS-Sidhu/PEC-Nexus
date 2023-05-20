cc32965a8bd0c7b6c94d5d38f5cfee95f1dfe1c7
13jksingh <jaskeerat56.93@gmail.com>
2023-05-20 21:51:40 +0530

    Cards added in a row

--- src/components/Resources/Cards.js
+++ src/components/Resources/Cards.js
@@ -1,5 +1,5 @@
 export default function Card(props) {
-    return (<div style={{padding:20}}>
+    return (<div style={{padding:20,float:"left"}}>
         {/* <div className="card card-compact w-96 bg-base-100 shadow-xl">
             <figure><img src="https://images.unsplash.com/photo-1680792563719-288027b2a090?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Shoes" /></figure>
             <div className="card-body">
