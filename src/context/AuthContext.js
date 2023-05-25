eadefa0d690815deb18a782c95a2cc168f8a5871
13jksingh <jaskeerat56.93@gmail.com>
2023-05-25 15:09:34 +0530

    bugs fixed for above commit feature

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -27,6 +27,19 @@ export const AuthProvider = ({ children }) => {
     }
   }, [])
 
+  const getURL = (path) => {
+    let url =
+      process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      'http://localhost:3000/';
+    // Make sure to include `https://` when not localhost.
+    url = url.includes('http') ? url : `https://${url}`;
+    // Make sure to including trailing `/`.
+    url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+    url = url+path.substr(1);
+    console.log(url);
+    return url;
+  };
+
   const signInWithGoogle = async (path) => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
@@ -35,7 +48,7 @@ export const AuthProvider = ({ children }) => {
           access_type:'offline',
           prompt: 'consent',
         },
-        redirectTo: path
+        redirectTo: getURL(path)
       },
     })
 
